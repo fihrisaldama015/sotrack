e63167d5ad83abd77de49af2c4dc14cc3d20fab2
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2024-04-16 11:41:31 +0700

    [ADD] auth facebook callback

---
+++ app/(pages)/auth/facebook/callback/page.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import LoadingSpinner from "@/app/components/LoadingSpinner";
+import { setCookie } from "cookies-next";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+const page = () => {
+  const searchParams = useSearchParams();
+  const facebook_user_id = searchParams.get("facebook_user_id");
+  const router = useRouter();
+  useEffect(() => {
+    setCookie("facebook_user_id", facebook_user_id, {
+      path: "/",
+      maxAge: 60 * 60 * 24 * 7,
+    });
+    router.push("/dashboard");
+  }, []);
+  return (
+    <div className="flex w-full h-[100svh] flex-col justify-center items-center">
+      <LoadingSpinner />
+      Redirecting...
+    </div>
+  );
+};
+
+export default page;
