f02d88c5cfb899f0c260eef18085e027b3720c0f
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2024-05-31 08:49:22 +0700

    [ADD] download report timeline function

--- app/(pages)/(dashboard)/dashboard/timeline/components/DownloadReportComponent.jsx
+++ app/(pages)/(dashboard)/dashboard/timeline/components/DownloadReportComponent.jsx
@@ -1,15 +1,42 @@
 "use client";
 import { DOWNLOAD } from "@/app/utils/assets";
 import { Box, Typography } from "@mui/material";
+import dayjs from "dayjs";
 import Image from "next/image";
 import React from "react";
 import { useSelector } from "react-redux";
 
 const DownloadReport = () => {
-  const { timelineData } = useSelector((state) => state.timelineDataReducer);
+  const { timelineData, selectedPlatform } = useSelector(
+    (state) => state.timelineDataReducer
+  );
   const download = () => {
     console.log("Download report => ", timelineData);
+    // console.log("Download report => ", user);
+    const csvContent =
+      "data:text/csv;charset=utf-8," +
+      "Id,Caption,Permalink,Source,Timestamp\n" +
+      timelineData
+        .map(
+          (post) =>
+            `${post.id},${post.caption},${post.permalink},${
+              post.source
+            },"${dayjs(post.timestamp).format("YYYY-MM-DD HH:mm:ss")}"`
+        )
+        .join("\n");
+    const encodedUri = encodeURI(csvContent);
+    const link = document?.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute(
+      "download",
+      `Socialens - Timeline ${selectedPlatform} - ${dayjs().format(
+        "YYYY-MM-DD HH:mm:ss"
+      )}.csv`
+    );
+    document.body.appendChild(link);
+    link.click();
   };
+
   return (
     <Box
       onClick={download}
